d9fd7496ddafe1a04491aa087e0c28bb967df05f
mhmdfrhn <frhn2707@gmail.com>
2023-06-05 11:21:04 +0700

    rendered button & alert component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
-import React from "react";
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
 import Message from "./Message";
 
 const App = () => {
+  // const items = [
+  //   "indonesia",
+  //   "singapore",
+  //   "thailand",
+  //   "philipines",
+  //   "japan",
+  //   "korea",
+  // ];
+
+  // const handleSelectedItem = (item: string) => {
+  //   console.log(item);
+  // };
+
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
     <div>
-      <Message />
+      {/* <ListGroup
+        items={items}
+        heading="cities"
+        onSelectedItem={handleSelectedItem}
+      /> */}
+      {/* <Alert>
+        Hello <span>World</span>
+      </Alert> */}
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
+      )}
+      <Button color="danger" onClick={() => setAlertVisibility(true)}>
+        My Button
+      </Button>
     </div>
   );
 };
